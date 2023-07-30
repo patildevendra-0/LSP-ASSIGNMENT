e4ce93b2f5db889b51754577d5afcdcfeaad7d2f
patildevendra-0 <patildev2001@gmail.com>
2023-07-30 23:37:29 +0530

    new

--- Assignment7-2.c
+++ Assignment7-2.c
@@ -4,9 +4,32 @@
 int main()
 {
     void *ptr = NULL;
-    void 
+    void (*fptr)();
 
+    char FileName1[20];
+    char FileName2[20];
 
+    printf("ENTER THE FILE NAME: ");
+    scanf("%s",FileName1);
+
+    printf("ENTER THE FILE NAME: ");
+    scanf("%s",FileName2);
+
+    ptr = dlopen("/home/devendra/Desktop/LSP-ASSIGNMENT-1/FileCompare.so",RTLD_LAZY);
+    if(ptr == NULL)
+    {
+        printf("UNABLE TO LOAD LIBRARY..\n");
+        return -1;
+    }
+
+    fptr = dlsym(ptr,"FileCompare");
+    if(fptr == NULL)
+    {
+        printf("UNABLE TO FETCH ADRESS OF FUNCTIOn..\n");
+        return -1;
+    }
+
+    fptr(&FileName1,&FileName2);
 
     return 0;
 }
