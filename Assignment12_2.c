e9d58297dff712c09c4cd4d69516c57a318b6449
patildevendra-0 <patildev2001@gmail.com>
2023-08-13 20:48:40 +0530

    new

--- Assignment12_2.c
+++ Assignment12_2.c
@@ -1,27 +1,39 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<sys/shm.h>
+#include<string.h>
 
 int main()
 {
-    int ShmId = 0;
-    int Key = 1234;
-    int ShmSize = 100;
-    char * ptr = NULL;
+    int SHMID = 0;
+    int KEY = 1234;
+    int SHMSIZE = 100;
+    char *ptr = NULL;
 
-    ShmId = shmget(Key,ShmSize,IPC_CREAT|0666);
-    if(ShmId != -1)
+    int iCnt = 0;
+    int Length = 0;
+    char Arr[] = "JAY GANESH";
+
+    SHMID = shmget(KEY,SHMSIZE,IPC_CREAT|0666);
+    if(SHMID != -1)
     {
-        printf("SHARED MEMORY ALLOCATED SUCCESSFULLY..\n");
+        printf("SHARED MEMORY ALLOCATED SUCCESSFULLY : \n");
     }
 
-    ptr = shmat(ShmId,NULL,0);
+    ptr = shmat(SHMID,NULL,0);
     if(ptr != NULL)
     {
         printf("SHARED MEMORY ATTACH SUCCESSFULLY...\n");
     }
 
-    
+    Length = strlen(Arr);
+
+    for(iCnt = 0;iCnt<Length;iCnt++)
+    {
+        ptr[iCnt] = Arr[iCnt];
+    }
+
+    ptr[Length] = '\0';
 
     return 0;
 }
--- Assignment12_3.c
+++ Assignment12_3.c
@@ -1,23 +1,37 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<sys/shm.h>
+#include<string.h>
 
 #define BLOCKSIZE 512
 
 int main()
 {
-    int ShmId = 0;
-    int Key = 1234;
-    int ShmSize = 100;
+    int SHMID = 0;
+    int KEY = 1234;
+    int SHMSIZE = 100;
+    char *ptr = NULL;
     char Buffer[BLOCKSIZE];
+    int iCnt = 0;
 
-    ShmId = shmget(Key,ShmSize,0666);
-    if(ShmId != -1)
+    SHMID = shmget(KEY,SHMSIZE,0666);
+    if(SHMID != -1)
     {
-        printf("SHARED MEMORY ALLOCATED SUCCESSFULLY...\n");
+        printf("SHARED MEMORY ALLOCATED SUCCESSFULLY : \n");
     }
 
+    ptr = shmat(SHMID,NULL,0);
+    if(ptr != NULL)
+    {
+        printf("SHARED MEMORY ATTACH SUCCESSFULLY...\n");
+    }
 
+    while(ptr[iCnt] != '\0')
+    {
+        Buffer[iCnt] = ptr[iCnt];
+        iCnt++;
+    }
+    printf("DATA FROM SHARED MEMORY : %s\n",Buffer);
 
     return 0;
 }
